4335855b0ad9d38c47a00a49023d8b409a22f79e
Arpita Agarwal <21cse011@gweca.ac.in>
2024-07-26 00:14:45 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+import Restaurant from "./restaurant";
+
+const App = () => {
+  return <Restaurant />;
+};
+
+export default App;
---
+++ src/menuApi.js
@@ -0,0 +1,413 @@
+const Menu = [
+  {
+    id: 1,
+    image: "images/aloo gobhi.jpg",
+    name: "AALU GOBHI",
+    category: "MAIN COURSE",
+    price: "50₹",
+    description:
+      "Delight in our Aalu Gobhi, a traditional Indian dish combining potatoes and cauliflower in a savory blend of spices, cooked to perfection.",
+  },
+  {
+    id: 2,
+    image: "images/daal.jpg",
+    name: "DAAL",
+    category: "MAIN COURSE",
+    price: "80₹",
+    description:
+      "Savor our Daal, a hearty and flavorful lentil stew that's both comforting and nutritious, seasoned with authentic Indian spices.",
+  },
+  {
+    id: 3,
+    image: "images/matarpaneer.webp",
+    name: "MATAR PANEER",
+    category: "MAIN COURSE",
+    price: "60₹",
+    description:
+      "Enjoy our Matar Paneer, a rich and creamy dish featuring tender peas and paneer cheese simmered in a spiced tomato gravy.",
+  },
+  {
+    id: 4,
+    image: "images/soya_chunks_curry.jpg",
+    name: "SOYA CHUNKS CURRY",
+    category: "MAIN COURSE",
+    price: "10₹",
+    description:
+      "Try our Soya Chunks Curry, a delicious and protein-packed dish with soya chunks cooked in a flavorful curry sauce.",
+  },
+  {
+    id: 5,
+    image: "images/sev.jfif",
+    name: "SEV TAMATAR",
+    category: "MAIN COURSE",
+    price: "10₹",
+    description:
+      "Experience the tangy and spicy flavors of Sev Tamatar, a unique Gujarati dish made with crispy sev and a tomato-based curry.",
+  },
+  {
+    id: 6,
+    image: "images/chane.jfif",
+    name: "CHANA MASALA",
+    category: "MAIN COURSE",
+    price: "10₹",
+    description:
+      "Enjoy our Chana Masala, a classic Indian dish of chickpeas cooked in a spicy and tangy tomato-based sauce.",
+  },
+  {
+    id: 7,
+    image: "images/mix veg.jfif",
+    name: "MIX VEG",
+    category: "MAIN COURSE",
+    price: "10₹",
+    description:
+      "Taste our Mix Veg, a medley of fresh vegetables cooked in a flavorful blend of spices, perfect for a wholesome meal.",
+  },
+  {
+    id: 8,
+    image: "images/alumatar.jfif",
+    name: "ALOO MATAR",
+    category: "MAIN COURSE",
+    price: "10₹",
+    description:
+      "Delight in Aloo Matar, a comforting dish of potatoes and peas cooked in a savory spiced gravy.",
+  },
+  {
+    id: 9,
+    image: "images/gatte.jfif",
+    name: "GATTA CURRY",
+    category: "MAIN COURSE",
+    price: "10₹",
+    description:
+      "Savor the traditional flavors of Gatta Curry, with gram flour dumplings simmered in a tangy and spicy yogurt-based gravy.",
+  },
+  {
+    id: 10,
+    image: "images/mushroom.jfif",
+    name: "MATAR MUSHROOM",
+    category: "MAIN COURSE",
+    price: "10₹",
+    description:
+      "Enjoy our Matar Mushroom, a delightful combination of mushrooms and peas cooked in a spiced tomato gravy.",
+  },
+  {
+    id: 11,
+    image: "images/kofta.jfif",
+    name: "MALAI KOFTA",
+    category: "MAIN COURSE",
+    price: "10₹",
+    description:
+      "Indulge in Malai Kofta, soft and creamy kofta balls made with vegetables and paneer, served in a rich and creamy gravy.",
+  },
+  {
+    id: 12,
+    image: "images/steamed.jfif",
+    name: "STEAMED RICE",
+    category: "RICE",
+    price: "10₹",
+    description:
+      "Pair your meal with our perfectly steamed rice, a simple yet essential accompaniment to any main course.",
+  },
+  {
+    id: 13,
+    image: "images/fried rice.jpg",
+    name: "VEGETABLE FRIED RICE",
+    category: "RICE",
+    price: "180₹",
+    description:
+      "Try our Vegetable Fried Rice, a delicious blend of rice and fresh vegetables stir-fried with soy sauce and spices.",
+  },
+  {
+    id: 14,
+    image: "images/pulao.jfif",
+    name: "VEGETABLE PULAO",
+    category: "RICE",
+    price: "10₹",
+    description:
+      "Enjoy our Vegetable Pulao, a fragrant rice dish cooked with a mix of vegetables and aromatic spices.",
+  },
+  {
+    id: 15,
+    image: "images/mexican.jfif",
+    name: "MEXICAN RICE BOWL",
+    category: "RICE",
+    price: "10₹",
+    description:
+      "Savor our Mexican Rice Bowl, a fusion dish with rice, beans, vegetables, and bold Mexican flavors.",
+  },
+  {
+    id: 16,
+    image: "images/jeera.jfif",
+    name: "JEERA RICE",
+    category: "RICE",
+    price: "10₹",
+    description:
+      "Enjoy Jeera Rice, a simple and aromatic rice dish flavored with cumin seeds, perfect to pair with any curry.",
+  },
+  {
+    id: 17,
+    image: "images/dum.jfif",
+    name: "SPECIAL DUM BIRYANI",
+    category: "RICE",
+    price: "10₹",
+    description:
+      "Indulge in our Special Dum Biryani, a rich and flavorful rice dish with layers of fragrant basmati rice and spiced vegetables.",
+  },
+  {
+    id: 18,
+    image: "images/ice.jfif",
+    name: "ICE-CREAM",
+    category: "DESSERT",
+    price: "10₹",
+    description:
+      "Cool off with our creamy and delicious ice cream, available in a variety of flavors to satisfy your sweet tooth.",
+  },
+  {
+    id: 19,
+    image: "images/gulab.jfif",
+    name: "GULAB JAMUN",
+    category: "DESSERT",
+    price: "10₹",
+    description:
+      "Enjoy our Gulab Jamun, soft and spongy milk-solid-based sweets soaked in a fragrant sugar syrup.",
+  },
+  {
+    id: 20,
+    image: "images/walnut.jfif",
+    name: "HOT WALNUT BROWNIE",
+    category: "DESSERT",
+    price: "10₹",
+    description:
+      "Indulge in our Hot Walnut Brownie, a rich and gooey chocolate dessert topped with crunchy walnuts.",
+  },
+  {
+    id: 21,
+    image: "images/waffle.jfif",
+    name: "CHOCOLATE WAFFLE",
+    category: "DESSERT",
+    price: "10₹",
+    description:
+      "Treat yourself to our Chocolate Waffle, a crispy and decadent dessert drizzled with chocolate sauce.",
+  },
+  {
+    id: 22,
+    image: "images/bnana.jfif",
+    name: "BANANA AND NUTELLA WAFFLE",
+    category: "DESSERT",
+    price: "50₹",
+    description:
+      "Enjoy our Banana and Nutella Waffle, a delightful combination of fresh bananas and creamy Nutella on a warm waffle.",
+  },
+  {
+    id: 23,
+    image: "images/cheese.jfif",
+    name: "CHEESECAKE",
+    category: "DESSERT",
+    price: "50₹",
+    description:
+      "Indulge in our rich and creamy Cheesecake, a classic dessert with a smooth texture and delicious flavor.",
+  },
+  {
+    id: 24,
+    image: "images/shikanji.jfif",
+    name: "MASALA SHIKANJI",
+    category: "MOCKTAILS",
+    price: "50₹",
+    description:
+      "Refresh yourself with Masala Shikanji, a tangy and spicy Indian lemonade perfect for quenching your thirst.",
+  },
+  {
+    id: 25,
+    image: "images/lemona.jfif",
+    name: "LEMONANA",
+    category: "MOCKTAILS",
+    price: "50₹",
+    description:
+      "Try our Lemonana, a refreshing mocktail combining the flavors of lemon and mint for a cool and zesty drink.",
+  },
+  {
+    id: 26,
+    image: "images/blue.jfif",
+    name: "BLUE SKY",
+    category: "MOCKTAILS",
+    price: "50₹",
+    description:
+      "Sip on Blue Sky, a visually stunning and refreshing mocktail with a unique blend of tropical flavors.",
+  },
+  {
+    id: 27,
+    image: "images/watermelon.jfif",
+    name: "WATERMELON COOLER",
+    category: "MOCKTAILS",
+    price: "50₹",
+    description:
+      "Cool off with our Watermelon Cooler, a hydrating and refreshing drink perfect for hot days.",
+  },
+  {
+    id: 28,
+    image: "images/lime.jfif",
+    name: "FRESH LIME SODA",
+    category: "MOCKTAILS",
+    price: "50₹",
+    description:
+      "Enjoy Fresh Lime Soda, a fizzy and tangy drink that's perfect for a refreshing break.",
+  },
+  {
+    id: 29,
+    image: "images/milk.jfif",
+    name: "PLAIN MILK",
+    category: "MOCKTAILS",
+    price: "50₹",
+    description:
+      "Simple and nutritious, our Plain Milk is perfect for a wholesome and refreshing drink any time of the day.",
+  },
+  {
+    id: 31,
+    image: "images/paneer.jfif",
+    name: "PANEER TIKKA",
+    category: "STARTERS",
+    price: "150₹",
+    description:
+      "Enjoy Paneer Tikka, succulent pieces of paneer marinated in a blend of spices and grilled to perfection.",
+  },
+  {
+    id: 33,
+    image: "images/much.jfif",
+    name: "VEG MANCHURIAN",
+    category: "STARTERS",
+    price: "140₹",
+    description:
+      "Try Veg Manchurian, a popular Indo-Chinese dish featuring vegetable dumplings in a tangy and spicy sauce.",
+  },
+  {
+    id: 34,
+    image: "images/spring.jfif",
+    name: "SPRING ROLLS",
+    category: "STARTERS",
+    price: "120₹",
+    description:
+      "Crispy and delicious Spring Rolls filled with a savory mix of vegetables, served with a tangy dipping sauce.",
+  },
+  {
+    id: 35,
+    image: "images/tikki.jfif",
+    name: "ALOO TIKKI",
+    category: "STARTERS",
+    price: "100₹",
+    description:
+      "Enjoy Aloo Tikki, crispy and flavorful potato patties spiced with Indian herbs and served with chutney.",
+  },
+  {
+    id: 36,
+    image: "images/mango.jfif",
+    name: "MANGO SHAKE",
+    category: "BEVERAGES",
+    price: "60₹",
+    description:
+      "Refresh yourself with our Mango Shake, a creamy and delicious drink made with fresh mangoes and milk.",
+  },
+  {
+    id: 38,
+    image: "images/lassi.jfif",
+    name: "SWEET LASSI",
+    category: "BEVERAGES",
+    price: "40₹",
+    description:
+      "Enjoy Sweet Lassi, a creamy and refreshing yogurt-based drink sweetened to perfection.",
+  },
+  {
+    id: 41,
+    image: "images/roti.jfif",
+    name: "ROTI",
+    category: "BREADS",
+    price: "10₹",
+    description:
+      "Enjoy our soft and warm Roti, a staple Indian bread made from whole wheat flour.",
+  },
+  {
+    id: 42,
+    image: "images/naan.jfif",
+    name: "NAAN",
+    category: "BREADS",
+    price: "20₹",
+    description:
+      "Try our Naan, a fluffy and buttery Indian flatbread that's perfect for pairing with curries.",
+  },
+
+  {
+    id: 45,
+    image: "images/paratha.jfif",
+    name: "ALOO PARATHA",
+    category: "BREADS",
+    price: "35₹",
+    description:
+      "Enjoy Aloo Paratha, a stuffed flatbread filled with spiced potato mixture, served with yogurt or pickle.",
+  },
+
+  {
+    id: 53,
+    image: "images/bhurji.jfif",
+    name: "PANEER BHURJI",
+    category: "MAIN COURSE",
+    price: "140₹",
+    description:
+      "Savor Paneer Bhurji, scrambled paneer cooked with tomatoes, onions, and spices for a delicious and nutritious dish.",
+  },
+  {
+    id: 54,
+    image: "images/bhature.jfif",
+    name: "CHOLE BHATURE",
+    category: "MAIN COURSE",
+    price: "90₹",
+    description:
+      "Enjoy Chole Bhature, a popular Punjabi dish with spicy chickpeas served with fluffy deep-fried bread.",
+  },
+  {
+    id: 55,
+    image: "images/dhokla.jfif",
+    name: "DHOKLA",
+    category: "STARTERS",
+    price: "50₹",
+    description:
+      "Indulge in Dhokla, a soft and spongy steamed cake made from fermented rice and chickpea batter, garnished with mustard seeds.",
+  },
+  {
+    id: 56,
+    image: "images/rasgulla.jfif",
+    name: "RASGULLA",
+    category: "DESSERT",
+    price: "20₹",
+    description:
+      "Treat yourself to Rasgulla, soft and spongy cheese balls soaked in a light sugar syrup, offering a melt-in-your-mouth experience.",
+  },
+
+  {
+    id: 57,
+    image: "images/parathe.png",
+    name: "PARATHE",
+    category: "BREADS",
+    price: "20₹",
+    description: "Enjoy the paratha with the buttery makkhan on it.",
+  },
+
+  {
+    id: 58,
+    image: "images/maggi.jpg",
+    name: "NOODLES",
+    category: "STARTERS",
+    price: "20₹",
+    description:
+      "Enjoy the yummy noodles of different variety and customized according to you.",
+  },
+
+  {
+    id: 59,
+    image: "images/burger.jfif",
+    name: "BURGER",
+    category: "STARTERS",
+    price: "20₹",
+    description:
+      "Taste is best of our burger with lots of enjoyment and happiness in your lives.",
+  },
+];
+
+export default Menu;
